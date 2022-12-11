7fc9854f2c9d0359511010076422ac8fb611da89
Chetan1947 <chetanmohanpatil@gmail.com>
2022-12-11 13:27:02 +0530

    pydantic changes

---
+++ models/datamodels/characters.py
@@ -0,0 +1,83 @@
+'''
+{
+    "birth_year": "19 BBY",
+    "eye_color": "Blue",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        ...
+    ],
+    "gender": "Male",
+    "hair_color": "Blond",
+    "height": "172",
+    "homeworld": "https://swapi.dev/api/planets/1/",
+    "mass": "77",
+    "name": "Luke Skywalker",
+    "skin_color": "Fair",
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-10T13:52:43.172000Z",
+    "species": [
+        "https://swapi.dev/api/species/1/"
+    ],
+    "starships": [
+        "https://swapi.dev/api/starships/12/",
+        ...
+    ],
+    "url": "https://swapi.dev/api/people/1/",
+    "vehicles": [
+        "https://swapi.dev/api/vehicles/14/"
+        ...
+'''
+
+from models.basemodels import Base
+from typing import Optional, List
+class characters(Base):
+    birth_year: str
+    eye_color: str
+    films: Optional[List[str]]
+    gender: str
+    hair_color: str
+    height: str
+    homeworld: str
+    mass: str
+    name: str
+    skin_color: str
+    species: Optional[List[str]]
+    starships: Optional[List[str]]
+    vehicles: Optional[List[str]]
+
+
+var={
+    "name": "Luke Skywalker",
+    "height": "172",
+    "mass": "77",
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "19BBY",
+    "gender": "male",
+    "homeworld": "https://swapi.dev/api/planets/1/",
+    "films": [
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/6/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/7/"
+    ],
+    "species": [
+        "https://swapi.dev/api/species/1/"
+    ],
+    "vehicles": [
+        "https://swapi.dev/api/vehicles/14/",
+        "https://swapi.dev/api/vehicles/30/"
+    ],
+    "starships": [
+        "https://swapi.dev/api/starships/12/",
+        "https://swapi.dev/api/starships/22/"
+    ],
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-20T21:17:56.891000Z",
+    "url": "https://swapi.dev/api/people/1/"
+}
+
+var1=characters(**var)
+print(var1)
